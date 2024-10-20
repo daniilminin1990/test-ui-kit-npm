0a15d9cb626692eb2fa7d34486fe9683da6f2315
Daniil <daniilminin1990@yandex.ru>
2024-10-20 23:51:27 +0500

    fix: svgs and componsntsSvgs export

--- .rollup.cache/D/mine-projects/justForTests/testReactFeatures/test-ui-kit-npm/dist/components/button/Button.js
+++ .rollup.cache/D/mine-projects/justForTests/testReactFeatures/test-ui-kit-npm/dist/components/button/Button.js
@@ -4,7 +4,7 @@ import { clsx } from 'clsx';
 import s from './Button.module.scss';
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const ButtonPolymorph = (props, ref) => {
-    const { as: Component = 'button', className, fullWidth, variant = 'primary', children, ...rest } = props;
+    const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props;
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,react/react-in-jsx-scope
     return _jsx(Component, { className: clsx(s.button, s[variant], fullWidth && s.fullWidth, className), ref: ref, ...rest });
 };
